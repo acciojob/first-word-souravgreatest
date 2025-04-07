3a6c6068b45581c8617eefe500e12bb182d1e042
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-04-07 21:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	let ans="";
+	for(let ch of s)
+		{
+			if(ch!==" ")
+			{
+				ans+=ch;
+			}
+			else
+			{
+				if(ans.length!==0)break;
+			}
+		}
+	return ans; 
 }
 
 // Do not change the code below
